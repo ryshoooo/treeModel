02834f494b72af7553df07cfce24d90ecdc6626f
rnm <ryshoooo@gmail.com>
2018-10-22 20:07:53 +0200

    comments, more tests for data types

--- src/datamodel/datatypes.py
+++ src/datamodel/datatypes.py
@@ -1,6 +1,6 @@
 import numpy as np
 import collections
-from datetime import date
+from datetime import date, datetime
 
 
 class DataType(object):
@@ -49,7 +49,7 @@ class DataType(object):
         :param value: Value to be converted.
         :return: Converted value of the specific data type.
         """
-        return self.get_numpy_type().type(value)
+        return self.get_numpy_type().type(value).astype(self.get_numpy_type())
 
     def build_python_value(self, value):
         """
@@ -61,7 +61,16 @@ class DataType(object):
 
 
 class StringDataType(DataType):
+    """
+    DataType for string/categorical inputs.
+    """
+
     def __init__(self, nullable=True, longest_string=200):
+        """
+        Initialize the data type.
+        :param nullable: Boolean specifying whether the data type can contain missing values.
+        :param longest_string: Integer specifying the longest possible string input.
+        """
         if nullable:
             super(StringDataType, self).__init__('<U{}'.format(longest_string), str, 'nan', None)
         else:
@@ -69,7 +78,16 @@ class StringDataType(DataType):
 
 
 class FloatDataType(DataType):
+    """
+    DataType for float/continuous/discrete inputs.
+    """
+
     def __init__(self, nullable=True, bits=8):
+        """
+        Initialize the data type.
+        :param nullable: Boolean specifying whether the data type can contain missing values.
+        :param bits: Integer specifying the number of bits to allocate in the memory for the float.
+        """
         if nullable:
             super(FloatDataType, self).__init__('<f{}'.format(bits), float, np.nan, None)
         else:
@@ -77,35 +95,81 @@ class FloatDataType(DataType):
 
 
 class DateDataType(DataType):
-    def __init__(self, nullable=True, resolution='s'):
+    """
+    DataType for date/timestamp inputs.
+    """
+
+    def __init__(self, nullable=True, resolution='s', format_string="%Y-%m-%d %H:%M:%S.%f"):
+        """
+        Initialize Date DataType.
+        :param nullable: Boolean specifying whether the data type can contain missing values.
+        :param resolution: String specifying the wanted numpy resolution of the date type.
+        :param format_string: String Timestamp format.
+        """
         if nullable:
-            super(DateDataType, self).__init__('<M8[{}]'.format(resolution), date, np.datetime64('NaT'), None)
+            super(DateDataType, self).__init__('<M8[{}]'.format(resolution),
+                                               lambda x: datetime.strptime(x, format_string),
+                                               np.datetime64('NaT'), None)
         else:
-            super(DateDataType, self).__init__('<M8[{}]'.format(resolution), date, None, None)
+            super(DateDataType, self).__init__('<M8[{}]'.format(resolution),
+                                               lambda x: datetime.strptime(x, format_string),
+                                               None, None)
+
+    def build_numpy_value(self, value):
+        """
+        Method which converts the input value into the numpy type.
+        :param value: Value to be converted.
+        :return: Converted value of the specific data type.
+        """
+        return self.get_numpy_type().type(self.build_python_value(value))
 
 
 class ArrayDataType(DataType):
+    """
+    DataType for arrays (lists of single type).
+    """
+
     def __init__(self, element_data_type, nullable=True):
+        """
+        Initialize the data type.
+        :param element_data_type: DataType specifying the data type of the array elements.
+        :param nullable: Boolean specifying whether the data type can contain missing values.
+        """
         if not isinstance(element_data_type, DataType):
             raise AttributeError("The array element has to be of DataType instance!")
 
         self.element_data_type = element_data_type
+        self.element_numpy_type = element_data_type.get_numpy_type()
 
         if nullable:
             super(ArrayDataType, self).__init__(np.ndarray, list,
-                                                np.array([], dtype=np.dtype(element_data_type.numpy_dtype)), [])
+                                                np.array([], dtype=self.element_numpy_type), [])
         else:
             super(ArrayDataType, self).__init__(np.ndarray, list, None, None)
 
     def build_numpy_value(self, value):
+        """
+        Method which converts the input value into the numpy type.
+        :param value: Value to be converted.
+        :return: Converted value of the specific data type.
+        """
         if not isinstance(value, (collections.Sequence, np.ndarray)) or isinstance(value, str):
             raise AttributeError("Incorrect format of input value!")
 
-        return super(ArrayDataType, self).build_numpy_value(value).astype(self.element_data_type.get_numpy_type())
+        return self.get_numpy_type().type(value).astype(self.element_numpy_type)
 
 
 class ListDataType(DataType):
+    """
+    DataType for lists/trees (list with elements of different data types)
+    """
+
     def __init__(self, element_data_types, nullable=True):
+        """
+        Initialize the data type.
+        :param element_data_types: List/Sequence of DataTypes
+        :param nullable: Boolean specifying whether the data type can contain missing values.
+        """
         if not isinstance(element_data_types, (collections.Sequence, np.ndarray)) or isinstance(element_data_types,
                                                                                                 str):
             raise AttributeError("Incorrect format of input element data types!")
@@ -115,22 +179,31 @@ class ListDataType(DataType):
                 raise AttributeError("Elements of the list have to be of DataType instance!")
 
         self.element_data_types = element_data_types
+        self.element_numpy_types = self._get_numpy_dtypes()
 
         if nullable:
-            super(ListDataType, self).__init__(np.ndarray, list, np.empty((0,),
-                                                                          dtype=self._get_numpy_dtypes()), [])
+            super(ListDataType, self).__init__(np.ndarray, list, np.empty((0,), dtype=self.element_numpy_types), [])
         else:
             super(ListDataType, self).__init__(np.ndarray, list, None, None)
 
     def _get_numpy_dtypes(self):
+        """
+        Helper method to build input numpy dtypes for numpy structured array.
+        :return: List of tuples of format (String of index, String of numpy DType)
+        """
         return [('{}'.format(x), self.element_data_types[x].get_numpy_type()) for x in
                 range(len(self.element_data_types))]
 
     def build_numpy_value(self, value):
+        """
+        Method which converts the input value into the numpy type.
+        :param value: Value to be converted.
+        :return: Converted value of the specific data type.
+        """
         if not isinstance(value, (collections.Sequence, np.ndarray)) or isinstance(value, str):
             raise AttributeError("Incorrect format of input value!")
 
         input_values = tuple([self.element_data_types[x].build_numpy_value(value[x])
                               for x in range(len(self.element_data_types))])
 
-        return np.array(input_values, dtype=self._get_numpy_dtypes())
+        return np.array(input_values, dtype=self.element_numpy_types)
--- test/datamodel/test_dataType.py
+++ test/datamodel/test_dataType.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 import numpy as np
 
-from src.datamodel.datatypes import DataType
+from src.datamodel.datatypes import DataType, StringDataType
 
 
 class TestDataType(TestCase):
@@ -36,8 +36,6 @@ class TestDataType(TestCase):
         self.assertEqual(dtp.build_numpy_value(1), np.int32(1))
         dtp = DataType(numpy_dtype='<c16', python_dtype=float, numpy_na_value=None, python_na_value=None)
         self.assertEqual(dtp.build_numpy_value(1.1), np.complex128(1.1))
-        dtp = DataType(numpy_dtype='<a25', python_dtype=bytes, numpy_na_value=None, python_na_value=None)
-        self.assertEqual(dtp.build_numpy_value(10), np.bytes0(10))
 
     def test_build_python_value(self):
         dtp = DataType(numpy_dtype='<i4', python_dtype=int, numpy_na_value=None, python_na_value=None)
@@ -46,3 +44,47 @@ class TestDataType(TestCase):
         self.assertEqual(dtp.build_python_value(1.1), float(1.1))
         dtp = DataType(numpy_dtype='<a25', python_dtype=bytes, numpy_na_value=None, python_na_value=None)
         self.assertEqual(dtp.build_python_value(10), bytes(10))
+
+
+class TestStringDataType(TestCase):
+    """
+    Test class for StringDataType.
+    """
+
+    def test_is_nullable(self):
+        dtp = StringDataType(nullable=False)
+        self.assertFalse(dtp.is_nullable())
+        dtp = StringDataType(nullable=True)
+        self.assertTrue(dtp.is_nullable())
+
+    def test_get_numpy_type(self):
+        dtp = StringDataType(longest_string=10)
+        self.assertEqual(dtp.get_numpy_type(), np.dtype('<U10'))
+        dtp = StringDataType(longest_string=1)
+        self.assertEqual(dtp.get_numpy_type(), np.dtype('<U1'))
+        dtp = StringDataType()
+        self.assertEqual(dtp.get_numpy_type(), np.dtype('<U200'))
+
+    def test_get_python_type(self):
+        dtp = StringDataType(longest_string=10)
+        self.assertEqual(dtp.get_python_type(), str)
+        dtp = StringDataType(longest_string=11)
+        self.assertEqual(dtp.get_python_type(), str)
+        dtp = StringDataType()
+        self.assertEqual(dtp.get_python_type(), str)
+
+    def test_build_numpy_value(self):
+        dtp = StringDataType(longest_string=10)
+        self.assertEqual(dtp.build_numpy_value("1234567890123"), "1234567890")
+        dtp = StringDataType(longest_string=1)
+        self.assertEqual(dtp.build_numpy_value("123"), "1")
+        dtp = StringDataType()
+        self.assertEqual(dtp.build_numpy_value("tra2"), "tra2")
+
+    def test_build_python_value(self):
+        dtp = StringDataType(longest_string=10)
+        self.assertEqual(dtp.build_python_value(10), "10")
+        dtp = StringDataType(longest_string=1)
+        self.assertEqual(dtp.build_python_value(10), "10")
+        dtp = StringDataType()
+        self.assertEqual(dtp.build_python_value("tra2"), "tra2")
