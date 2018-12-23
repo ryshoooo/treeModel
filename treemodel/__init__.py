ec3813f16a3647750509289e06456508095103f8
rnm <ryshoooo@gmail.com>
2018-12-23 12:37:00 +0100

    treemodel docstring fixed

--- treemodel/__init__.py
+++ treemodel/__init__.py
@@ -1,6 +1,9 @@
 """
-Declares as a python module.
-Main root directory with the full path to the library's modules.
+This module contains the whole functionality of the :mod:`treemodel` library.
+
+The library is currently divided into 2 submodules:
+    - :mod:`treemodel.datamodel`: Contains all the functionality for building and parsing trees from the input data to the :class:`treemodel.datamodel.base.TreeDataSet`.
+    - :mod:`treemodel.linear_model`: Contains functionality for building standard sklearn linear models on TreeDataSets.
 """
 from . import datamodel
 
